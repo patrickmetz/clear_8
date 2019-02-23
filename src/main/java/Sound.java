431a2674f8f18b4814a2dfb67a443bda62b03d5f
Patrick Metz <patrickmetz@web.de>
2019-02-23 04:56:25 +0100

    - refactored sound

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -9,28 +9,22 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
-/**
- *
- */
 class Sound {
 
-    private static final boolean BIG_ENDIAN = false;
-    private static final int CHANNELS = 1;
-    private static final int HERTZ = 350;
-    private static final int MILLISECONDS = 500;
-    private static final float SAMPLE_RATE = 8000f;
-    private static final int SAMPLE_SIZE_IN_BITS = 8;
-    private static final boolean SIGNED = true;
+    private static final float SAMPLE_RATE_IN_HZ = 1000f;
+    private static final int TONE_DURATION_IN_MS = 1000;
+    private static final int TONE_FREQUENCY_IN_HZ = 400;
+    private static final int TONE_VOLUME = 100;
 
     private static AudioFormat audioFormat;
 
     Sound() {
         audioFormat = new AudioFormat(
-                SAMPLE_RATE,
-                SAMPLE_SIZE_IN_BITS,
-                CHANNELS,
-                SIGNED,
-                BIG_ENDIAN);
+                SAMPLE_RATE_IN_HZ,
+                8,
+                1,
+                true,
+                false);
     }
 
     void play() throws LineUnavailableException {
@@ -41,8 +35,8 @@ class Sound {
         sdl.open(audioFormat);
         sdl.start();
 
-        for (int i = 0; i < MILLISECONDS * SAMPLE_SIZE_IN_BITS; i++) {
-            buffer[0] = (byte) (sine(i) * 127.0);
+        for (int i = 0; i < TONE_DURATION_IN_MS * SAMPLE_RATE_IN_HZ / 1000; i++) {
+            buffer[0] = (byte) (sine(i));
             sdl.write(buffer, 0, buffer.length);
         }
 
@@ -51,7 +45,8 @@ class Sound {
         sdl.close();
     }
 
-    private double sine(int i) {
-        return Math.sin(i / (SAMPLE_RATE / HERTZ) * 2.0 * Math.PI);
+    private double sine(double i) {
+        return Math.sin(
+                i / (SAMPLE_RATE_IN_HZ / TONE_FREQUENCY_IN_HZ) * 2.0 * Math.PI) * TONE_VOLUME;
     }
 }
