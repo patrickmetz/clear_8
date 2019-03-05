1f011fa4ba9c610ebee6f3350d2ef733c121261b
Patrick Metz <patrickmetz@web.de>
2019-03-05 10:43:13 +0100

    - commented graphics

--- src/main/java/de/patrickmetz/bean8/emulator/hardware/Graphics.java
+++ src/main/java/de/patrickmetz/bean8/emulator/hardware/Graphics.java
@@ -1,6 +1,6 @@
 /*
  * Developed by Patrick Metz <patrickmetz@web.de>.
- * Last modified 05.03.19 09:58.
+ * Last modified 05.03.19 10:41.
  * Copyright (c) 2019. All rights reserved.
  */
 
@@ -22,26 +22,36 @@ final class Graphics {
         initializeScreenData();
     }
 
-    boolean drawSprite(int offsetX, int offsetY, int[] rows) {
+    boolean drawSprite(int offsetX, int offsetY, int[] spriteRows) {
         boolean collision = false;
 
         for (int x = 0; x < SPRITE_WIDTH; x++) {
-            for (int y = 0; y < rows.length; y++) {
-                // subsequently shifts each sprite bit, from index 7 to 0,
-                // to the rightmost position, and grabs and compares it from there with 1
-                boolean newPixelState =
-                        ((rows[y] >> (SPRITE_WIDTH - x - 1)) & 1) == 1;
+            for (int y = 0; y < spriteRows.length; y++) {
+                /* subsequently shifts each sprite bit, from index 7 to 0
+                (i.e. 8th to 1st), to the rightmost position, grabs it from
+                there and checks if it is equal to 1 (should be drawn) */
+                boolean pixelState =
+                        ((spriteRows[y] >> (SPRITE_WIDTH - x - 1)) & 0b00000001) == 1;
 
-
+                /* puts pixels on their absolute screen position and "wraps
+                them around", when they exceed screen width or height (e.g.
+                width position 65 becomes 1, 66 becomes 2, 67 becomes 3, ...).
+                So sprites exceeding the screen size re-enter it from the
+                opposite side.
+                 */
                 int positionX = (offsetX + x) % SCREEN_WIDTH;
                 int positionY = (offsetY + y) % SCREEN_HEIGHT;
 
-                if ((newPixelState) && (screenData[positionX][positionY])) {
-                    newPixelState = false;
+                /* The pixels are dawn using XOR logic: They must exclusively
+                be active in this draw call or in the last draw call, but not
+                in both. If they're active in both, they won't be drawn and
+                we register a collision. */
+                if ((pixelState) && (screenData[positionX][positionY])) {
+                    pixelState = false;
                     collision = true;
                 }
 
-                screenData[positionX][positionY] = newPixelState;
+                screenData[positionX][positionY] = pixelState;
             }
         }
 
